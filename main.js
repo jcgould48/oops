4d00d954128e22a418149089b93d0fd869ff29a3
Jesse Gould <jesse.gould@codeimmersives.com>
2019-11-27 13:35:52 -0500

    finished

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const Stack = function() {
   return {
     items : [],
+    add: function(name){
+    return this.items.push(name);
+    },
+    remove: function(name){
+    return this.items.pop(name);
+    },
+    peek: function(){
+      return this.items[this.items.length-1];
+    },
   }
-  items.push();
+  
 }
 
 const Queue = function() {
+  return {
+    items : [],
+    add: function(name){
+      return this.items.push(name);
+      },
+      remove: function(name){
+      return this.items.shift(name);
+      },
+      peek: function(){
+        return this.items[0];
+      },
+  }
 }
 
 module.exports = {
